1551dad461b23b136aa07cbaa211b8f18a285389
Andrey Kalashnikov <kalashnikow@mail.ru>
2018-12-13 10:45:00 +0300

    Большая картинка масштабируется

--- js/upload.js
+++ js/upload.js
@@ -1,11 +1,12 @@
 'use strict';
 
 var ESC_KEYCODE = 27;
-// var ENTER_KEYCODE = 13;
+var ENTER_KEYCODE = 13;
 
 var imgUpload = document.querySelector('.img-upload');
 var imgUploadOverlay = imgUpload.querySelector('.img-upload__overlay');
 var imgUploadCancel = imgUploadOverlay.querySelector('.img-upload__cancel');
+var imgUploadPreview = imgUploadOverlay.querySelector('.img-upload__preview');
 
 var uploadFile = imgUpload.querySelector('#upload-file');
 
@@ -17,43 +18,50 @@ var openUploadOverlay = function () {
 var closeUploadOverlay = function () {
   imgUploadOverlay.classList.add('hidden');
   document.removeEventListener('keydown', onUploadOverlayEcsPress);
+  cleanOverlayData();
 };
 
 var onUploadOverlayEcsPress = function (evt) {
   if (evt.keyCode === ESC_KEYCODE) {
     imgUploadOverlay.classList.add('hidden');
+    cleanOverlayData();
   }
 };
 
-uploadFile.addEventListener('change', openUploadOverlay);
+var cleanOverlayData = function () {
+  uploadFile.value = uploadFile.defaultValue;
+  imgUploadPreview.querySelector('img').style.transform = 'scale(1)';
+  scaleControlValue.value = '100%';
+  scaleControlValue.setAttribute('value', '100%');
+  console.log(uploadFile, scaleControlValue);
+};
 
 imgUploadCancel.addEventListener('click', closeUploadOverlay);
 imgUploadCancel.addEventListener('keydown', function (evt) {
-  if (evt.keyCode === ESC_KEYCODE) {
+  if (evt.keyCode === ENTER_KEYCODE) {
     closeUploadOverlay();
   }
 });
 
-var scaleControlValue = imgUploadOverlay.querySelector('.scale__control--value');
+uploadFile.addEventListener('change', openUploadOverlay);
+
+var imgUploadScale = imgUploadOverlay.querySelector('.img-upload__scale');
+var scaleControlValue = imgUploadScale.querySelector('.scale__control--value');
 var numericScaleControlValue = parseInt(scaleControlValue.value, 10);
 
-imgUploadOverlay.addEventListener('click', function (evt) {
+imgUploadScale.addEventListener('click', function (evt) {
   var target = evt.target;
   if (target.classList.contains('scale__control--smaller')) {
     numericScaleControlValue = Math.max(0, numericScaleControlValue - 25);
-    scaleControlValue.value = numericScaleControlValue + '%';
   } else if (target.classList.contains('scale__control--bigger')) {
     numericScaleControlValue = Math.min(100, numericScaleControlValue + 25);
-    scaleControlValue.value = numericScaleControlValue + '%';
   }
+  scaleControlValue.value = numericScaleControlValue + '%';
+  scaleControlValue.setAttribute('value', numericScaleControlValue + '%');
 
-  // При повторном открытии рисунка и предыдущем значении контрола не 50, показывается все равно 50
+  var scale = 'scale(' + numericScaleControlValue / 100 + ')';
+  imgUploadPreview.querySelector('img').style.transform = scale;
 
-  var imgUploadPreview = imgUploadOverlay.querySelector('.img-upload__preview');
-
-  scaleControlValue.addEventListener('input', function () {
-    console.log('+++');
-    imgUploadPreview.classList.add('effect-level transform: scale(0.75)');
-  });
-
+  console.log(uploadFile, scaleControlValue);
 });
+
